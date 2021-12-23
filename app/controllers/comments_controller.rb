b81c819fe3368800235543ddceea6a28b8d5f516
Nauman-Arshad <naumankhan642@gmail.com>
2021-12-23 16:07:11 +0500

    add comments on posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+  before_action :set_post
+
+  def create
+    @post.comments.create! params.require(:comment).permit(:content)
+    redirect_to @post
+  end
+  private
+  def set_post
+    @post =Post.find(params[:post_id])
+  end
 end
